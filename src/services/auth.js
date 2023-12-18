6ee179943838c19a7fb870c3a7f293c19d89b3e3
Nghiêm Hồng <hung24855@gmail.com>
2023-12-18 22:58:57 +0700

    Viet API register and login

---
+++ src/services/auth.js
@@ -0,0 +1,81 @@
+import db from "../models";
+import bcrypt, { genSaltSync } from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { v4 } from "uuid";
+require("dotenv").config();
+
+const hashPassword = (password) => bcrypt.hashSync(password, genSaltSync(12));
+
+export const registerServices = ({ name, password, phone }) =>
+  new Promise(async (resolve, reject) => {
+    try {
+      const response = await db.User.findOrCreate({
+        where: { phone },
+        defaults: {
+          phone,
+          name,
+          password: hashPassword(password),
+          id: v4(),
+        },
+      });
+      const token =
+        response[1] &&
+        jwt.sign(
+          {
+            id: response[0].id,
+            phone: response[0].phone,
+          },
+          process.env.SECRET_KEY,
+          {
+            expiresIn: "2d",
+          }
+        );
+
+      resolve({
+        err: token ? 0 : 2,
+        message: token
+          ? "Register is successfully"
+          : "Phone number has been aldreadly used !",
+        token: token || null,
+      });
+    } catch (error) {
+      reject(error);
+    }
+  });
+
+export const loginServices = ({ password, phone }) =>
+  new Promise(async (resolve, reject) => {
+    try {
+      const response = await db.User.findOne({
+        where: { phone },
+        raw: true, // Phải có cài này thì sequelize mới trả ra object dữ liệu
+      });
+      const isCorrectPassword =
+        response && bcrypt.compareSync(password, response.password);
+
+      const token =
+        isCorrectPassword &&
+        jwt.sign(
+          {
+            id: response.id,
+            phone: response.phone,
+          },
+          process.env.SECRET_KEY,
+          {
+            expiresIn: "2d",
+          }
+        );
+
+      resolve({
+        err: token ? 0 : 2,
+        message: token
+          ? "Login is successfully"
+          : response
+          ? "Password is wrong !"
+          : "Phone number not found !",
+        token: token || null,
+      });
+    } catch (error) {
+      reject(error);
+    }
+  });
